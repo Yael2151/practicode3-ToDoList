3bd5993b3fc027ca21d538e13825e1fa7e885a6a
Yael2151 <yael215128@gmail.com>
2025-02-06 18:23:59 +0200

    first time

---
+++ ToDoClient/src/service.js
@@ -0,0 +1,58 @@
+import axios from 'axios';
+
+const apiUrl = "http://localhost:5076"
+axios.defaults.baseURL = apiUrl; 
+
+axios.interceptors.response.use(
+  response => response,
+  error => {
+    
+    console.error('Error response:', error.response);
+    return Promise.reject(error);
+  }
+);
+
+export default {
+  getTasks: async () => {
+    try{
+      const result = await axios.get(`${apiUrl}/items`)    
+      return result.data;
+    }catch (error) {
+      console.error('Failed to fetch tasks:', error);
+      throw error;  // נחזיר את השגיאה למי שקרא לפונקציה
+    }
+  },
+
+  addTask: async(name)=>{
+    try {
+      const result = await axios.post('/items', { name });
+      console.log('Task added:', result.data);
+      return result.data;
+    } catch (error) {
+      console.error('Failed to add task:', error);
+      throw error;
+    }
+  },
+
+  setCompleted: async(id, isComplete)=>{
+    try {
+      const result = await axios.put(`/items/${id}`, { isComplete });
+      console.log('Task updated:', result.data);
+      return result.data;
+    } catch (error) {
+      console.error('Failed to update task:', error);
+      throw error;
+    }
+  },
+
+  deleteTask:async(id)=>{
+    try {
+      const result = await axios.delete(`/items/${id}`);
+      console.log('Task deleted:', result.data);
+      return result.data;
+    } catch (error) {
+      console.error('Failed to delete task:', error);
+      throw error;
+    }
+  }
+};
