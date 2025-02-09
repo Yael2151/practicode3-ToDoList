9d96cc24929808381b4263596989c89bd9077dfd
Yael2151 <yael215128@gmail.com>
2025-02-09 04:10:05 +0200

    change App

--- ToDoClient/src/App.js
+++ ToDoClient/src/App.js
@@ -5,55 +5,28 @@ function App() {
   const [newTodo, setNewTodo] = useState("");
   const [todos, setTodos] = useState([]);
 
-  // פונקציה לשליפת המשימות
   async function getTodos() {
-    try {
-      const todos = await service.getTasks();
-      if (Array.isArray(todos)) {
-        setTodos(todos); // עדכון רשימת המשימות
-      } else {
-        console.error("הנתונים שהתקבלו אינם מערך", todos);
-      }
-    } catch (error) {
-      console.error("שגיאה בשליפת המשימות", error);
-    }
+    const todos = await service.getTasks();
+    setTodos(todos);
   }
 
-  // פונקציה להוספת משימה חדשה
   async function createTodo(e) {
     e.preventDefault();
-    if (newTodo.trim()) { // בדיקה אם הקלט לא ריק
-      try {
-        await service.addTask(newTodo);
-        setNewTodo(""); // ניקוי שדה הקלט
-        await getTodos(); // עדכון רשימת המשימות
-      } catch (error) {
-        console.error("שגיאה בהוספת המשימה", error);
-      }
-    }
+    await service.addTask(newTodo);
+    setNewTodo("");//clear input
+    await getTodos();//refresh tasks list (in order to see the new one)
   }
 
-  // עדכון מצב המשימה (האם הושלמה או לא)
   async function updateCompleted(todo, isComplete) {
-    try {
-      await service.setCompleted(todo.id, isComplete);
-      await getTodos(); // עדכון רשימת המשימות
-    } catch (error) {
-      console.error("שגיאה בעדכון המשימה", error);
-    }
+    await service.setCompleted(todo.id, isComplete);
+    await getTodos();//refresh tasks list (in order to see the updated one)
   }
 
-  // מחיקת משימה
   async function deleteTodo(id) {
-    try {
-      await service.deleteTask(id);
-      await getTodos(); // עדכון רשימת המשימות
-    } catch (error) {
-      console.error("שגיאה במחיקת המשימה", error);
-    }
+    await service.deleteTask(id);
+    await getTodos();//refresh tasks list
   }
 
-  // קריאה לפונקציה לשליפת המשימות כאשר הקומפוננטה נטענת
   useEffect(() => {
     getTodos();
   }, []);
@@ -63,26 +36,16 @@ function App() {
       <header className="header">
         <h1>todos</h1>
         <form onSubmit={createTodo}>
-          <input
-            className="new-todo"
-            placeholder="Well, let's take on the day"
-            value={newTodo}
-            onChange={(e) => setNewTodo(e.target.value)}
-          />
+          <input className="new-todo" placeholder="Well, let's take on the day" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
         </form>
       </header>
       <section className="main" style={{ display: "block" }}>
         <ul className="todo-list">
-          {Array.isArray(todos) && todos.map(todo => {
+          {todos.map(todo => {
             return (
               <li className={todo.isComplete ? "completed" : ""} key={todo.id}>
                 <div className="view">
-                  <input
-                    className="toggle"
-                    type="checkbox"
-                    defaultChecked={todo.isComplete}
-                    onChange={(e) => updateCompleted(todo, e.target.checked)}
-                  />
+                  <input className="toggle" type="checkbox" defaultChecked={todo.isComplete} onChange={(e) => updateCompleted(todo, e.target.checked)} />
                   <label>{todo.name}</label>
                   <button className="destroy" onClick={() => deleteTodo(todo.id)}></button>
                 </div>
@@ -91,11 +54,110 @@ function App() {
           })}
         </ul>
       </section>
-    </section>
+    </section >
   );
 }
 
 export default App;
+
+// import React, { useEffect, useState } from 'react';
+// import service from './service.js';
+
+// function App() {
+//   const [newTodo, setNewTodo] = useState("");
+//   const [todos, setTodos] = useState([]);
+
+//   // פונקציה לשליפת המשימות
+//   async function getTodos() {
+//     try {
+//       const todos = await service.getTasks();
+//       if (Array.isArray(todos)) {
+//         setTodos(todos); // עדכון רשימת המשימות
+//       } else {
+//         console.error("הנתונים שהתקבלו אינם מערך", todos);
+//       }
+//     } catch (error) {
+//       console.error("שגיאה בשליפת המשימות", error);
+//     }
+//   }
+
+//   // פונקציה להוספת משימה חדשה
+//   async function createTodo(e) {
+//     e.preventDefault();
+//     if (newTodo.trim()) { // בדיקה אם הקלט לא ריק
+//       try {
+//         await service.addTask(newTodo);
+//         setNewTodo(""); // ניקוי שדה הקלט
+//         await getTodos(); // עדכון רשימת המשימות
+//       } catch (error) {
+//         console.error("שגיאה בהוספת המשימה", error);
+//       }
+//     }
+//   }
+
+//   // עדכון מצב המשימה (האם הושלמה או לא)
+//   async function updateCompleted(todo, isComplete) {
+//     try {
+//       await service.setCompleted(todo.id, isComplete);
+//       await getTodos(); // עדכון רשימת המשימות
+//     } catch (error) {
+//       console.error("שגיאה בעדכון המשימה", error);
+//     }
+//   }
+
+//   // מחיקת משימה
+//   async function deleteTodo(id) {
+//     try {
+//       await service.deleteTask(id);
+//       await getTodos(); // עדכון רשימת המשימות
+//     } catch (error) {
+//       console.error("שגיאה במחיקת המשימה", error);
+//     }
+//   }
+
+//   // קריאה לפונקציה לשליפת המשימות כאשר הקומפוננטה נטענת
+//   useEffect(() => {
+//     getTodos();
+//   }, []);
+
+//   return (
+//     <section className="todoapp">
+//       <header className="header">
+//         <h1>todos</h1>
+//         <form onSubmit={createTodo}>
+//           <input
+//             className="new-todo"
+//             placeholder="Well, let's take on the day"
+//             value={newTodo}
+//             onChange={(e) => setNewTodo(e.target.value)}
+//           />
+//         </form>
+//       </header>
+//       <section className="main" style={{ display: "block" }}>
+//         <ul className="todo-list">
+//           {Array.isArray(todos) && todos.map(todo => {
+//             return (
+//               <li className={todo.isComplete ? "completed" : ""} key={todo.id}>
+//                 <div className="view">
+//                   <input
+//                     className="toggle"
+//                     type="checkbox"
+//                     defaultChecked={todo.isComplete}
+//                     onChange={(e) => updateCompleted(todo, e.target.checked)}
+//                   />
+//                   <label>{todo.name}</label>
+//                   <button className="destroy" onClick={() => deleteTodo(todo.id)}></button>
+//                 </div>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       </section>
+//     </section>
+//   );
+// }
+
+// export default App;
 
 
 
--- TodoServer/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ TodoServer/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d173e7b445fbc83a7b103049ab59cb60656f0646")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdad21db663c5e3a1f583e18da9f273222bdfa4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
